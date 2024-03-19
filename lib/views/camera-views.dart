40d91b5edbbca1e94b7901c46f5986ed42c75150
omule0 <106811183+omule0@users.noreply.github.com>
2024-03-19 20:49:27 +0300

    Add files via upload

---
+++ lib/views/camera-views.dart
@@ -0,0 +1,22 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vid/controller/scan_controller.dart';
+
+class CameraView extends StatelessWidget {
+  const CameraView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GetBuilder<ScanController>(
+        init: ScanController(),
+        builder: (controller) {
+          return controller.isCameraInitialized.value
+              ? CameraPreview(controller.cameraController)
+              : const Center(child: Text("Loading Preview ..."));
+        },
+      ),
+    );
+  }
+}
